4a4107bb5aaf362392b29f98eec7d8797e9818f8
mathg <mathieu.grauwin1@gmail.com>
2018-05-17 00:07:58 +0200

    Ajout du code du projet

---
+++ dominion/card/ActionCard.java
@@ -0,0 +1,15 @@
+package dominion.card;
+import java.util.*;
+
+import dominion.*;
+
+/**
+ * Les cartes Action
+ */
+public abstract class ActionCard extends Card {
+
+	public ActionCard(String name, int cost) {
+		super(name, cost);
+		// TODO Auto-generated constructor stub
+	}
+}
