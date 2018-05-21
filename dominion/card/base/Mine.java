17ee24d78826cc04f9f3f07cb7fa561b892897a0
mathg <mathieu.grauwin1@gmail.com>
2018-05-21 21:31:33 +0200

    modif pour test

--- dominion/card/base/Mine.java
+++ dominion/card/base/Mine.java
@@ -16,22 +16,26 @@ public class Mine extends ActionCard {
 
 	@Override
 	public void play(Player p) {
+		
 		if(!p.getTreasureCards().isEmpty()) {
 			String cardName = p.chooseCard("Choisissez une carte Treasure de votre main à écarter : ", p.getTreasureCards(), false);
 			int cost = p.getHand().getCard(cardName).getCost();
-			p.getGame().trashCard(p.getHand().getCard(cardName));
+			p.getGame().trashCard(p.getHand().remove(cardName));
 			
+			CardList availableSupplyCards = p.getGame().availableSupplyCards();
 			CardList availableTreasureCards = new CardList();
-			for(int i = 0; i < p.getGame().availableSupplyCards().size() ; i++) { //on sélectionne chaque carte Treasure de la pile coûtant jusqu'à 3 Pièces de plus que la carte qu'il a écarté 
-				if(p.getGame().availableSupplyCards().get(i).getTypes().contains(CardType.Treasure) && p.getGame().availableSupplyCards().get(i).getCost() <= cost + 3) {
-					availableTreasureCards.add(p.getGame().availableSupplyCards().get(i));
+			for(int i = 0; i < availableSupplyCards.size() ; i++) { //on sélectionne chaque carte Treasure de la pile coûtant jusqu'à 3 Pièces de plus que la carte qu'il a écarté 
+				if(availableSupplyCards.get(i).getTypes().contains(CardType.Treasure) && availableSupplyCards.get(i).getCost() <= cost + 3) {
+					availableTreasureCards.add(availableSupplyCards.get(i));
 				}
 			}
 			
 			cardName = p.chooseCard("Choisissez le nom d'une Treasure à recevoir : ", availableTreasureCards, false);
 			p.getHand().add(p.getGame().removeFromSupply(cardName));
 		}
-
+		else {
+			System.out.println("Vous n'avez pas de carte Treasure à écarter...");
+		}
 		
 	}
 }
